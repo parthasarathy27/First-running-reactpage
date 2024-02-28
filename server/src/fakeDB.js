d71eeaba69ba504701a32f28e6d4e5e2a82611b8
Parthasarathy G <83574852+parthasarathy27@users.noreply.github.com>
2024-02-28 13:33:54 +0530

    Add files via upload

---
+++ server/src/fakeDB.js
@@ -0,0 +1,7 @@
+exports.fakeDB = [
+    {
+      id: 0,
+      email: 'thomas.weibenfalk@gmail.com',
+      password: '$2a$10$QOdIWzdNe9mkwQCD9H1tIeCKDp9JH1zw8J7qz8SXzJgQDr9kx2oue', // Password: test
+    },
+  ];
